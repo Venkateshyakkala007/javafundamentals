4f5038fff56d66ad4e21c68ac4e8949527b178ba
Venkatesh Yakkala <87487796+Venkateshyakkala007@users.noreply.github.com>
2024-03-01 23:49:48 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,118 @@
+
+
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        //1. take input from 
+        // ans : int n = sc.nextInt();
+
+
+        // Palindrom : reverse == orignal 
+        // 1221 =o rev = 1221 palindorm 
+
+        // int n = 1221;
+        // // palindorm number
+        // int rev = 0;
+
+        // while(n > 0){
+        //     int last = n%10;
+
+        //     rev = rev*10+last;
+        //     n = n/10;
+        // }
+        // if (n == rev){
+        //     System.out.println("It is a palindorm ");
+        // } else {
+        //     System.out.println("It is not palindorm");
+        // }
+
+
+
+        /*
+         * 
+         */
+        // int n = 4;
+
+        //  for(int i = 0;i <=n; i++){
+        //     for(int j = n-1; j>= i;j-- )
+        //     {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        //  }
+
+        // Amstrong number :
+         
+        // int n = 153; 
+        // int temp = n;
+        // int alpha = n;
+        // // finding the count of number
+
+        // System.out.println(n);
+
+        // int count = 0;
+
+        // while(n > 0){
+        //     count++;
+        //     n = n /10;
+        // }
+
+        // // System.out.println(n);
+
+        // System.out.println(count);
+
+        // int sum = 0;
+
+        // while(temp > 0){
+        //     int last = temp %10 ;
+        //     sum = sum + (int)Math.pow(last,count);
+        //     temp = temp/10;
+
+        // }
+
+        // if(alpha == sum){
+        //     System.out.println("Amstrong number");
+        // } else {
+        //     System.out.println("Not a amstrong number");
+        // }
+
+
+        // fibnacci  \
+
+        // int n= 5;
+
+        // int[] a = new int[n];
+
+        // a[0]= 0;
+        // a[1] = 1;
+
+        // for(int i =2; i < n; i++){
+        //     a[i] = a[i-2]+a[i-1];
+        // }
+
+        // System.out.println("These are fibanacci number");
+        // System.out.println();
+
+        // for(int i = 0; i < n; i++)
+        // {
+        //     System.out.print(a[i] +" ");
+        // }
+
+        // // How to take elements in array
+
+        // int[] arr = new int[7];
+
+        // for(int i = 0; i< 7; i++)
+        // {
+        //     arr[i] = sc.nextInt();
+        // }
+
+    }
+}
